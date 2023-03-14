f0e3a0e32f3773c4b31346e7db777a87a85fc063
VerneriL <44767@edu.raseko.fi>
2023-03-14 15:12:47 +0200

    tests for dict method

--- test_questions.py
+++ test_questions.py
@@ -1,27 +1,15 @@
 import questions
-import pytest
 
-# def test_get_input_integer(monkeypatch):
-#     user_input = '100'
-#     monkeypatch.setattr('builtins.input', lambda _:user_input)
-#     question = questions.Question('Luku: ')
-#     assert question.get_input_integer(False) == (100, 'OK', 0, 'Conversion successful')
-
-# def test_get_input_integer_2(monkeypatch):
-#     user_input='100 v'
-#     monkeypatch.setattr('builtins.input', lambda _:user_input)
-#     question = questions.Question('Luku: ')
-#     assert question.get_input_integer(False) == (0, 'Error', 1, "invalid literal for int() with base 10: '100 v'")
 
 def test_static_get_user_integer(monkeypatch):
     user_input='100'
     monkeypatch.setattr('builtins.input', lambda _:user_input)
-    assert questions.Question.get_user_integer('Luku: ', False) == (100, 'OK', 0, 'Conversion successful')
+    assert questions.Question.get_input_integer('Luku: ', False) == (100, 'OK', 0, 'Conversion successful')
 
 def test_static_get_user_integer_2(monkeypatch):
     user_input='100 v'
     monkeypatch.setattr('builtins.input', lambda _:user_input)
-    assert questions.Question.get_user_integer('Luku: ', False) == (0, 'Error', 1, "invalid literal for int() with base 10: '100 v'")
+    assert questions.Question.get_input_integer('Luku: ', False) == (0, 'Error', 1, "invalid literal for int() with base 10: '100 v'")
 
 def test_static_get_input_boolean(monkeypatch):
     user_input='y'
@@ -38,14 +26,22 @@ def test_static_get_input_float(monkeypatch):
     monkeypatch.setattr('builtins.input', lambda _:user_input)
     assert questions.Question.get_input_float('Luku: ', False) == (1.5, 'OK', 0, 'Conversion successful')
 
-def test_static_get_input_float_error(monkeypatch):
+def test_static_get_input_float_replace(monkeypatch):
     user_input = "74,6"
     monkeypatch.setattr('builtins.input', lambda _:user_input)
-    assert questions.Question.get_input_float('Luku: ', False) == (0, 'Error', 1, "could not convert string to float: '74,6'")
+    assert questions.Question.get_input_float('Luku: ', False) == (74.6, 'OK', 0, 'Conversion successful')
 
 def test_static_get_input_float_error_2(monkeypatch):
     user_input = "1.5v"
     monkeypatch.setattr('builtins.input', lambda _:user_input)
     assert questions.Question.get_input_float('Luku: ', False) == (0, 'Error', 1, "could not convert string to float: '1.5v'")
 
+def test_ask_user_dictionary(monkeypatch):
+    user_input = "Tyttö"
+    monkeypatch.setattr('builtins.input', lambda _:user_input)
+    assert questions.Question.ask_user_dictionary('Sukupuoli: ', {"tyttö": 0}, False) == (0, 'OK', 0,  'Conversion successful')
 
+def test_ask_user_dictionary_error(monkeypatch):
+    user_input = "auto"
+    monkeypatch.setattr('builtins.input', lambda _:user_input)
+    assert questions.Question.ask_user_dictionary('Sukupuoli: ', {"tyttö": 0}, False) == ('N/A', 'Error', 1, "'auto'")
